ed2125f12a23fdaa32dba401c33d2dda9b6f07cb
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-05-30 09:17:06 +0200

    MEP éléments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarrouselComponent } from './carrousel/carrousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    CarrouselComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/carrousel/carrousel.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-carrousel",
+  templateUrl: "./carrousel.component.html",
+  styleUrls: ["./carrousel.component.css"]
+})
+export class CarrouselComponent implements OnInit {
+  
+  constructor() {}
+
+  images = [1, 2, 3].map(
+    () => `https://picsum.photos/900/500?random&t=${Math.random()}`
+  );
+
+  ngOnInit() {}
+}
