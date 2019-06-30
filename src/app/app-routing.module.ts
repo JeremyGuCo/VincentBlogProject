53e93677db56acc318271bf29f55de615059f762
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-06-30 10:50:01 +0200

    Réorganisation des dossiers + Changement styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { NewsComponent } from './news/news.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { NewsComponent } from './features/news/news.component';
+import { PublicationsComponent } from './features/publications/publications.component';
+import { BibliographyComponent } from './features/bibliography/bibliography.component';
+
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'home', component:HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'news', component: NewsComponent}
+  {path: 'news', component: NewsComponent},
+  {path: 'publications', component: PublicationsComponent},
+  {path: 'bibliography', component: BibliographyComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { CarrouselComponent } from './carrousel/carrousel.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { NewsComponent } from './news/news.component';
-import { PublicationsComponent } from './publications/publications.component';
-import { BibliographyComponent } from './bibliography/bibliography.component';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavBarComponent,
-    CarrouselComponent,
-    FooterComponent,
-    HeaderComponent,
-    AboutComponent,
-    NewsComponent,
-    PublicationsComponent,
-    BibliographyComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    CoreModule,
+    SharedModule,
+    FeaturesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
